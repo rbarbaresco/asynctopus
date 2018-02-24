e8f489b94377da407865c44d87704650af24ad25
rbarbaresco <barbaresco.rodrigo@gmail.com>
2018-02-24 14:01:14 -0300

    Added rabbitmq libraries.

--- server.go
+++ server.go
@@ -1,25 +1,33 @@
+/*
+https://hub.docker.com/_/redis/
+docker run --name some-redis -d redis
+*/
 package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"net/http"
+	//"time"
+	"log"
 
 	"github.com/graphql-go/graphql"
-	"github.com/mitchellh/mapstructure"
+	//"github.com/adjust/rmq"
+	"github.com/streadway/amqp"
 )
 
 type request struct {
-	method string// `json:"name"`
-	url   string// `json:"id"`
-	body   string// `json:"id"`
-	headers   string// `json:"id"`
-	callback_url   string// `json:"id"`
-	pid   string// `json:"id"`
+	method string
+	url   string
+	body   string
+	headers   string
+	callback_url   string
+	pid   string
+	//executing bool
 }
 
-var tasks_queue map[string]request
+var tasks_queue []request
 
 /*
    Create User object type with fields "id" and "name" by using GraphQLObjectTypeConfig:
@@ -89,9 +97,37 @@ var queryType = graphql.NewObject(
 					},
 				},
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					fmt.Println("Came here :)")
-					fmt.Println(p.Args)
-					return "Some random pid.", nil
+					fmt.Println("Came here :) req2")
+
+					var req request = request{}
+
+					if val, ok := p.Args["method"].(string); ok {
+						req.method = val
+					}
+
+					if val, ok := p.Args["url"].(string); ok {
+						req.url = val
+					}
+
+					if val, ok := p.Args["body"].(string); ok {
+						req.body = val
+					}
+
+					if val, ok := p.Args["headers"].(string); ok {
+						req.headers = val
+					}
+
+					if val, ok := p.Args["callback_url"].(string); ok {
+						req.callback_url = val
+					}
+
+					req.pid = "SOME_RANDOM PID"
+					
+
+
+					var reque = append(tasks_queue, req)
+					fmt.Println(tasks_queue)
+					return reque, nil
 					/*idQuery, isOK := p.Args["id"].(string)
 					if isOK {
 						return data[idQuery], nil
@@ -120,6 +156,65 @@ func execute(query string, schema graphql.Schema) *graphql.Result {
 }
 
 func main() {
+	
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	
+	q, err := ch.QueueDeclare(
+	  "hello", // name
+	  false,   // durable
+	  false,   // delete when unused
+	  false,   // exclusive
+	  false,   // no-wait
+	  nil,     // arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+
+	// BEGIN publishing to queue:
+	body := "hello"
+	err = ch.Publish(
+	  "",     // exchange
+	  q.Name, // routing key
+	  false,  // mandatory
+	  false,  // immediate
+	  amqp.Publishing {
+	    ContentType: "text/plain",
+	    Body:        []byte(body),
+	  })
+	failOnError(err, "Failed to publish a message")
+	// END publishing to queue.
+
+	// BEGIN Consuming queue:
+	msgs, err := ch.Consume(
+	  q.Name, // queue
+	  "",     // consumer
+	  true,   // auto-ack
+	  false,  // exclusive
+	  false,  // no-local
+	  false,  // no-wait
+	  nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+	  for d := range msgs {
+	    log.Printf("Received a message: %s", d.Body)
+	  }
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+	// END Consuming queue:
+
 	//_ = importJSONDataFromFile("data.json", &data)
 
 	http.HandleFunc("/execute", func(w http.ResponseWriter, r *http.Request) {
@@ -127,6 +222,7 @@ func main() {
 		result := execute(r.URL.Query().Get("request"), schema)
 		json.NewEncoder(w).Encode(result)
 		fmt.Println("Finalizend")
+		fmt.Println(tasks_queue)
 	})
 
 	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
@@ -138,6 +234,13 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+func failOnError(err error, msg string) {
+  if err != nil {
+    fmt.Printf("%s: %s", msg, err)
+    panic(fmt.Sprintf("%s: %s", msg, err))
+  }
+}
+/*
 //Helper function to import json from file to map
 func importJSONDataFromFile(fileName string, result interface{}) (isOK bool) {
 	isOK = true
@@ -153,3 +256,4 @@ func importJSONDataFromFile(fileName string, result interface{}) (isOK bool) {
 	}
 	return
 }
+*/
